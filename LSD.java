f21aea721cef90ba22bf01edf1b9bfcac77da4e4
Dmitry <ivanov.inin.dmitry@gmail.com>
2015-09-22 22:56:08 +0400

    End of work

--- LSD.java
+++ LSD.java
@@ -6,10 +6,11 @@ public class LSD {
 
 	private static final int SIZE = 1000000;
 	private static Random random = new Random();
-
+	private static final int W = 4;								// because int is 4 bytes
+	
 	public static void main(String[] args)
 	{
-		int[] data = generate();
+		int[] data = generate();	
 		int [] data2 = data.clone(); 
 		
 		long start = System.nanoTime();
@@ -17,17 +18,61 @@ public class LSD {
 		long stop = System.nanoTime();
 
 		System.out.println("Elapsed = " + (stop - start));
-
+		long first = stop - start;
+		
 		start = System.nanoTime();
 		LSDsort(data2);
 		stop = System.nanoTime();
+		
+		System.out.println("Elapsed = " + (stop - start) );
+		long second = stop - start;
+		
+		if(check(data2)){
+			System.out.println("LSD sort is fasted in "  + (first * 1.0 / second) + " times");
+		}
 
-		System.out.println("Elapsed = " + (stop - start));
-
-}
+	}
 
 	private static void LSDsort(int[] data) {
-	
+		int n = data.length;
+		int r = 256;
+		int mask = r - 1;
+		
+		int[] arr = new int [n];
+		
+		for(int i = 0; i < W; i++)
+		{
+			int[] count = new int [r+1];
+			for(int j = 0; j < n; j++)
+			{
+				int c = (data[j] >> 8 * i) & mask;
+				count[c + 1]++;
+			}
+			for(int k = 0; k < r; k++)								//compute frequency counts
+			{
+				count[k + 1] += count[k];
+			}
+			if (i == W-1) 
+			{
+                int shift1 = count[r] - count[r / 2];
+                int shift2 = count[r / 2];
+                for (int k = 0; k < r / 2; k++)
+                {
+                	count[k] += shift1;
+                }
+                for (int k = r / 2; k < r; k++)
+                {
+                	count[k] -= shift2;
+                }
+            }
+			for(int l = 0; l < n; l++)								//move data
+			{
+				int c = (data[l] >> 8 * i) & mask;
+				arr[count[c]++] = data[i];
+			}
+			for (int l = 0; l < n; l++)								//copy back
+				 data[l] = arr[l];
+		}
 	}
 
 	private static int[] generate() {
@@ -39,6 +84,16 @@ public class LSD {
 		}
 		
 		return data;
+	}
+	
+	private static boolean check( int[] data ){
+		
+		for(int i = 0; i < data.length - 1; i++)
+		{
+			if(data[i] > data[i+1])
+				return false;
+		}
+		return true;
 	}
 	
 }
